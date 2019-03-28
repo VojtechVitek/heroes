ea07a95eb8ffb05653e4b2b149999f93fae6f5b3
Vojtech Vitek <vojtech.vitek@gmail.com>
2019-03-28 01:01:34 -0400

    Tabs comments table is not readable on Github

--- pkg/mp2/header.go
+++ pkg/mp2/header.go
@@ -18,31 +18,31 @@ import (
 // struct padding whenever it feels like when you
 // combine uint16 and uint8. ¯\_(ツ)_/¯
 //
-// 0x0		0		MagicByte			4 bytes
-// 0x4		4		Difficulty			2 bytes
-// 0x6		6		Width				1 byte
-// 0x7		7		Height				1 byte
-// 0x8		8		KingdomColors		6 bytes
-// 0xE		14		AllowHumanColors	6 bytes
-// 0x14		20		AllowAIColors		6 bytes
-// 0x1A		26		_ (Kingdom Count?)	3 bytes
-// 0x1D		29		VictoryConditions	1 byte
-// 0x1E		30		AIAlsoWins			1 byte
-// 0x1F		31		AllowNormalVictory	1 byte
-// 0x20		32		VictoryData1		2 bytes
-// 0x22		34		LossConditions		1 byte
-// 0x23		35		LossData1			2 bytes
-// 0x25		37		StartWithHeroes		1 byte
-// 0x26		38		Races				6 bytes
-// 0x2C		44		VictoryData2		2 bytes
-// 0x2e		46		LossData2			2 bytes
-// 0x30		48		_					10 bytes
-// 0x3A		58		Name				16 bytes
-// 0x4A		74		_					44 bytes
-// 0x76		118		Description			143 bytes
-// 0x105	261		_					159 bytes
-// 0x1A4	420		Width (duplicate)	4 bytes
-// 0x1A8	424		Height (duplicate)	4 bytes
+// 0x0       0     MagicByte           4 bytes
+// 0x4       4     Difficulty          2 bytes
+// 0x6       6     Width               1 byte
+// 0x7       7     Height              1 byte
+// 0x8       8     KingdomColors       6 bytes
+// 0xE      14     AllowHumanColors    6 bytes
+// 0x14     20     AllowAIColors       6 bytes
+// 0x1A     26     _ (Kingdom Count?)  3 bytes
+// 0x1D     29     VictoryConditions   1 byte
+// 0x1E     30     AIAlsoWins          1 byte
+// 0x1F     31     AllowNormalVictory  1 byte
+// 0x20     32     VictoryData1        2 bytes
+// 0x22     34     LossConditions      1 byte
+// 0x23     35     LossData1           2 bytes
+// 0x25     37     StartWithHeroes     1 byte
+// 0x26     38     Races               6 bytes
+// 0x2C     44     VictoryData2        2 bytes
+// 0x2e     46     LossData2           2 bytes
+// 0x30     48     _                   10 bytes
+// 0x3A     58     Name                16 bytes
+// 0x4A     74     _                   44 bytes
+// 0x76    118     Description         143 bytes
+// 0x105   261     _                   159 bytes
+// 0x1A4   420     Width (duplicate)   4 bytes
+// 0x1A8   424     Height (duplicate)  4 bytes
 type Header [428]byte
 
 func LoadHeader(r io.Reader) (*Header, error) {
