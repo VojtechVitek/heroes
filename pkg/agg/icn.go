9186bfcb9705dcab76ab014c658662db0fa5c43a
Vojtech Vitek <vojtech.vitek@gmail.com>
2019-04-22 22:08:20 -0400

    Rename SpriteHeader to Sprite (will parse data too); remove NumSprites

--- pkg/agg/icn.go
+++ pkg/agg/icn.go
@@ -22,7 +22,7 @@ func NewICN(data []byte, pallete pallete) (*ICN, error) {
 	if err := binary.Read(bytes.NewReader(data[0:2]), binary.LittleEndian, &u16); err != nil {
 		return nil, errors.Wrap(err, "failed to read number of sprites")
 	}
-	icn.NumSprites = int(u16)
+	numSprites := int(u16)
 
 	var u32 uint32
 	if err := binary.Read(bytes.NewReader(data[2:6]), binary.LittleEndian, &u32); err != nil {
@@ -35,32 +35,34 @@ func NewICN(data []byte, pallete pallete) (*ICN, error) {
 		return nil, errors.Errorf("expected data size %v bytes, got %v bytes", dataSize, len(icn.data))
 	}
 
-	spriteHeaders := make([]*SpriteHeader, 0, icn.NumSprites)
-	for i := 0; i < icn.NumSprites; i++ {
-		header, err := NewSpriteHeader(icn.data[i*13 : (i+1)*13])
+	sprites := make([]*Sprite, 0, numSprites)
+	for i := 0; i < numSprites; i++ {
+		header, err := NewSprite(icn.data[i*13 : (i+1)*13])
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to parse sprite header")
 		}
-		spriteHeaders = append(spriteHeaders, header)
+		sprites = append(sprites, header)
 	}
-	icn.spriteHeaders = spriteHeaders
+	icn.sprites = sprites
 
 	return &icn, nil
 }
 
 type ICN struct {
-	NumSprites    int
-	spriteHeaders []*SpriteHeader
-	data          []byte
-	pallete       pallete
+	sprites []*Sprite
+	data    []byte
+	pallete pallete
 }
 
-type SpriteHeader struct {
+type Sprite struct {
 	x      int
 	y      int
 	width  int
 	height int
 	typ    uint8
+	// index int
+	// count int
+	// data []byte
 	dataAt int
 }
 
@@ -70,13 +72,13 @@ type SpriteHeader struct {
 // 0x06  height                 2 bytes
 // 0x08  type                   1 byte
 // 0x09  data offset            4 bytes
-func NewSpriteHeader(data []byte) (*SpriteHeader, error) {
+func NewSprite(data []byte) (*Sprite, error) {
 	if len(data) != 13 {
 		return nil, errors.Errorf("expected 13 bytes of sprite header, got %v bytes", len(data))
 	}
 
 	var (
-		h   SpriteHeader
+		h   Sprite
 		s16 int16
 		u16 uint16
 		u32 uint32
@@ -113,13 +115,14 @@ func NewSpriteHeader(data []byte) (*SpriteHeader, error) {
 }
 
 func (icn *ICN) Images() ([]*image.RGBA, error) {
-	images := make([]*image.RGBA, 0, icn.NumSprites)
+	images := make([]*image.RGBA, 0, len(icn.sprites))
 
-	for _, h := range icn.spriteHeaders {
+	for _, h := range icn.sprites {
 		rect := image.Rect(0, 0, h.width, h.height)
 
+		// TODO: 6 bytes is header of the data; u16 count + u32 data size
 		parser := &spriteParser{
-			data:      icn.data[h.dataAt:],
+			data:      icn.data[h.dataAt+6:],
 			lineWidth: h.width,
 			pallete:   icn.pallete,
 			pixels:    make([]uint8, 0, 4*h.width*h.height),
@@ -134,9 +137,9 @@ func (icn *ICN) Images() ([]*image.RGBA, error) {
 func (icn *ICN) String() string {
 	var b strings.Builder
 
-	fmt.Fprintf(&b, "Number of sprites: %v\n", icn.NumSprites)
+	fmt.Fprintf(&b, "Number of sprites: %v\n", len(icn.sprites))
 
-	for _, header := range icn.spriteHeaders {
+	for _, header := range icn.sprites {
 		fmt.Fprintf(&b, "%+v\n", header)
 	}
 
